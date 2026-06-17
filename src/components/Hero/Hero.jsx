fa906f65be18edc06b33c0423ea9b1c3ad93093b
GJSB <gerrard.barrows@gmail.com>
2026-06-17 16:25:40 -0400

    create hero section

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,27 @@
+import './Hero.css'
+
+function Hero()
+{
+    return (
+        <div className='hero-section'>
+            <img src="/images/page-graphic.png" alt="" />
+            <div className='hero-layout'>
+                <div className='hero-content'>
+                    <img className='tornado' src="/images/tornado.png" alt="" />
+                    <img className='crosses' src="/images/crosses.png" alt="" />
+                    <img className='crosses2' src="/images/crosses.png" alt="" />
+                    <h1>Gerrard <br />J.S <br /> Barrows</h1>
+                    <h2> Technical Project Manager <br /> - <br />  Digital Project Coordinator</h2>
+                </div>
+                <div className='hero-profile'>
+                    <img className='arrow' src="/images/arrow.png" alt="" />
+                    <div className="profile-card">
+                        <img src="/images/Profile.png" alt="" />
+                    </div>
+                </div>
+            </div>  
+        </div>
+    )
+}
+
+export default Hero
